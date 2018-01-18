febec549d77f27e0651653c729a33b676f810a9d
inzaghihao <wangyipippo@163.com>
2018-01-18 18:35:43 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # xcx-happyapp
 this is the happy app
 
+![happy-app](./proImg/demo1.png)
 
 #### 实现功能
 
 - swiper 做tab导航切换
-- 调用api渲染列表数据
+- 调用代理接口api渲染列表数据
 - 列表下拉加载
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,10 @@ import config from '../../utils/config.js'
 import util from '../../utils/util.js'
 import Mock from '../mock/mock.js'
 const app = getApp()
+const baseApi = 'https://www.yingshangyan.com/api/commen/getUrlContent';
 const RequestApi = 'https://route.showapi.com/255-1';
+const girlApi = 'http://gank.io/api/data/福利/20/';
+const randomgirlApi = 'http://gank.io/api/random/data/福利/20'
 const urlParams = {
   showapi_appid: "54360",
   showapi_sign: "28bd99df05ef49dca94cf538276fc278"
@@ -54,6 +57,10 @@ Page({
     this.setData({
       currentTab: e.detail.current
     });
+    if (this.data.currentTab == 1) {
+      this.getGirlData();
+      return
+    }
     //如果需要加载数据
     if (this.needLoadNewDataAfterSwiper()) {
       this.refreshNewData();
@@ -63,10 +70,43 @@ Page({
     this.setData({
       currentTab: e.target.dataset.id
     });
+    if (this.data.currentTab == 1) {
+      this.getGirlData();
+      return
+    }
     //如果需要加载数据
     if (this.needLoadNewDataAfterSwiper()) {
       this.refreshNewData();
     }
+  },
+  getGirlData(){
+    let that = this;
+    page = 1;
+    let girlApis = `${girlApi}${page}`
+    let girlApiBase64 = util.Base64.encode(girlApis)
+    console.log(girlApis)
+    util.request({
+      url: baseApi,
+      data: {
+        url: girlApiBase64,
+        type: 'get'
+      }
+    }).then(res => {
+      let dataList = res.data.results;
+      let imgList = dataList.map(item => {
+        return item.url
+      })
+      that.setData({
+        imgList
+      })
+      that.setData({
+        girlList: res.data.results
+      })
+      setTimeout(() => {
+        wx.hideNavigationBarLoading();
+        wx.hideToast();
+      }, 1000);
+    })
   },
   imgYu(event) {
     var src = event.currentTarget.dataset.src;//获取data-src
@@ -96,9 +136,23 @@ Page({
     util.showLoading();
     wx.showNavigationBarLoading();
     page = 1;
-    let url = `${RequestApi}?showapi_appid=${urlParams.showapi_appid}&showapi_sign=${urlParams.showapi_sign}&type=${types[this.data.currentTab]}&page=${page}`;
+    let params = {
+      'showapi_appid': urlParams.showapi_appid,
+      'showapi_sign': urlParams.showapi_sign,
+      'type': types[this.data.currentTab],
+      'page':page
+    }
+    let paramsStr = JSON.stringify(params)
+    let paramsBase64 = util.Base64.encode(paramsStr)
+    let RequestApiBase64 = util.Base64.encode(RequestApi)
+    console.log(paramsStr)
     util.request({
-      url: url,
+      url: baseApi,
+      data:{
+        url: RequestApiBase64,
+        type:'get',
+        params: paramsBase64
+      }
     }).then(res => {
       console.log(res)
       that.setNewDataWithRes(res, that);
@@ -109,7 +163,7 @@ Page({
     })
   },
   setNewDataWithRes(res,that){
-    var dataList = res.showapi_res_body.pagebean.contentlist;
+    var dataList = res.data.showapi_res_body.pagebean.contentlist;
     if (dataList[0].cdn_img){
       let imgList = dataList.map(item => {
         return item.cdn_img
@@ -127,6 +181,9 @@ Page({
         break;
       //视频
       case DATATYPE.VIDEODATATYPE:
+        dataList.forEach((item) => {
+          item.text = util.removeHTMLTag(item.text)
+        })
         that.setData({
           videoList: dataList
         });
@@ -161,11 +218,29 @@ Page({
   loadMoreData(){
     //加载提示框
     util.showLoading();
+    wx.showNavigationBarLoading();
     var that = this;
     page += 1;
-    let url = `${RequestApi}?showapi_appid=${urlParams.showapi_appid}&showapi_sign=${urlParams.showapi_sign}&type=${types[this.data.currentTab]}&page=${page}`;
+    if(this.data.currentTab == 1){
+      this.loadGirlData(page)
+      return
+    }
+    let params = {
+      'showapi_appid': urlParams.showapi_appid,
+      'showapi_sign': urlParams.showapi_sign,
+      'type': types[this.data.currentTab],
+      'page': page
+    }
+    let paramsStr = JSON.stringify(params)
+    let paramsBase64 = util.Base64.encode(paramsStr)
+    let RequestApiBase64 = util.Base64.encode(RequestApi)
     util.request({
-      url: url,
+      url: baseApi,
+      data: {
+        url: RequestApiBase64,
+        type: 'get',
+        params: paramsBase64
+      }
     }).then(res => {
       console.log(res)
       that.setMoreDataWithRes(res, that);
@@ -175,12 +250,54 @@ Page({
       }, 1000);
     })
   },
+  //加载更多妹子图
+  loadGirlData(page){
+    let that = this;
+    let girlApis = `${girlApi}${page}`
+    let girlApiBase64 = util.Base64.encode(girlApis)
+    console.log(girlApis)
+    util.request({
+      url: baseApi,
+      data: {
+        url: girlApiBase64,
+        type: 'get'
+      }
+    }).then(res => {
+      let dataList = res.data.results;
+      let _imgList = this.data.imgList;
+      if (_imgList) {
+        let imgList = dataList.map(item => {
+          return item.url
+        })
+        that.setData({
+          imgList: _imgList.concat(imgList)
+        })
+      }
+      that.setData({
+        girlList: that.data.girlList.concat(dataList)
+      })
+      setTimeout(() => {
+        wx.hideNavigationBarLoading();
+        wx.hideToast();
+      }, 1000);
+    })
+  },
+  //滑动到底部
   onReachBottom() {
     console.log(1)
   },
   //设置加载更多的数据
   setMoreDataWithRes(res, that) {
-    var dataList = res.showapi_res_body.pagebean.contentlist;
+    var dataList = res.data.showapi_res_body.pagebean.contentlist;
+    let _imgList = this.data.imgList;
+    if (_imgList){
+      let imgList = dataList.map(item => {
+        return item.image0
+      })
+      this.setData({
+        imgList: _imgList.concat(imgList)
+      })
+    }
     switch (types[this.data.currentTab]) {
       //段子
       case DATATYPE.TEXTDATATYPE:
@@ -204,4 +321,41 @@ Page({
         break;
     }
   },
+  randomLoad(){
+    let that = this;
+    let girlApiBase64 = util.Base64.encode(randomgirlApi)
+    page = Math.max(1,(Math.random()*10) | 0)
+    util.request({
+      url: baseApi,
+      data: {
+        url: girlApiBase64,
+        type: 'get'
+      }
+    }).then(res => {
+      console.log(res)
+      let dataList = res.data.results;
+      let imgList = dataList.map(item => {
+        return item.url
+      })
+      that.setData({
+        imgList
+      })
+      that.setData({
+        girlList: res.data.results
+      })
+      setTimeout(() => {
+        wx.hideNavigationBarLoading();
+        wx.hideToast();
+      }, 1000);
+    })
+  },
+  //视频播放开始
+  videoPlay(e){
+    //暂停上一条视频的播放
+    
+  },
+  //视频播放结束
+  videoEnd(){
+    
+  }
 })
